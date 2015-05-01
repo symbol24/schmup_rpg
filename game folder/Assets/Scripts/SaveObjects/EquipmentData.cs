92531911bf0f7a4f0f707491ddd2059ed031b45d
Victor <monstervictor@gmail.com>
2015-04-30 20:56:43 -0400

    Pulling and Adding the Generator

--- game folder/Assets/Scripts/SaveObjects/EquipmentData.cs
+++ game folder/Assets/Scripts/SaveObjects/EquipmentData.cs
@@ -24,12 +24,12 @@ public class EquipmentData {
 
 	public float[] m_ValueModifiers = new float[7];
     public float m_modifierDamage {get { return m_ValueModifiers[0]; } set { m_ValueModifiers[0] = value; }}
-    public float m_modifierFireRateModifier { get { return m_ValueModifiers[1]; } set { m_ValueModifiers[1] = value; } }
-    public float m_modifierSpeedModifier { get { return m_ValueModifiers[2]; } set { m_ValueModifiers[2] = value; } }
-    public float m_modifierArmourModifier { get { return m_ValueModifiers[3]; } set { m_ValueModifiers[3] = value; } }
-    public float m_modifierEnergyModifier { get { return m_ValueModifiers[4]; } set { m_ValueModifiers[4] = value; } }
-    public float m_modifierShieldModifier { get { return m_ValueModifiers[5]; } set { m_ValueModifiers[5] = value; } }
-    public float m_modifierHealthModifier { get { return m_ValueModifiers[6]; } set { m_ValueModifiers[6] = value; } }
+    public float m_modifierFireRate { get { return m_ValueModifiers[1]; } set { m_ValueModifiers[1] = value; } }
+    public float m_modifierSpeed { get { return m_ValueModifiers[2]; } set { m_ValueModifiers[2] = value; } }
+    public float m_modifierArmour { get { return m_ValueModifiers[3]; } set { m_ValueModifiers[3] = value; } }
+    public float m_modifierEnergy { get { return m_ValueModifiers[4]; } set { m_ValueModifiers[4] = value; } }
+    public float m_modifierShield { get { return m_ValueModifiers[5]; } set { m_ValueModifiers[5] = value; } }
+    public float m_modifierHealth { get { return m_ValueModifiers[6]; } set { m_ValueModifiers[6] = value; } }
 	
 
 }
