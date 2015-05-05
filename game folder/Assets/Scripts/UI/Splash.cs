ebcb643ce1d6d66f76ea4c360d4dd7fd6aaa0d90
BenGoulet <s4kur4@gmail.com>
2015-05-04 21:19:36 -0400

    Made a new Splash Screen

---
+++ game folder/Assets/Scripts/UI/Splash.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Splash : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		OnKeyDown ();
+	}
+
+	void OnKeyDown()
+	{
+		if (Input.GetKeyDown(KeyCode.Return))
+		{
+			Application.LoadLevel("loader"); 
+		}
+	}
+}
