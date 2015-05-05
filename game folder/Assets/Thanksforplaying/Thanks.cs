c3473552309e6b415ff5ad54ce0b7c18e0e1cc24
jeffcerto <jeffcertosini@icloud.com>
2015-05-04 21:30:41 -0400

    thank you screen

---
+++ game folder/Assets/Thanksforplaying/Thanks.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Thanks : MonoBehaviour 
+{
+	//public float delaytime = 5;
+
+    void Update()
+	{
+		onkeydown();
+	}
+	void onkeydown()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			Application.LoadLevel("loader");
+		}
+	}
+}
